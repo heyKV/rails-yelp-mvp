f9f331ec2a2d82a9ca438b07bbc50cf5d223c312
Frederic Kaveh <heydarkv@yahoo.fr>
2018-08-08 14:02:52 +0200

    restaurant table added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dragon Fury',
+    address:      'NoWhere in Barcelone',
+    phone_number: '01 43 54 45 31',
+    category:     'chinese'
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A man Street, Roma V1 6PJQ',
+    phone_number: '09 54 74 87 21',
+    category:     'italian'
+  },
+  {
+    name:         'Le bon petit bistrot',
+    address:      'NoWhere in Paris',
+    phone_number: '01 43 54 45 31',
+    category:     'french'
+  },
+  {
+    name:         'Un verre de trop',
+    address:      '56A Shoreditch High St, Belgium',
+    phone_number: '09 78 74 23 92',
+    category:     'belgian'
+  },
+  {
+    name:         'Lotus d argent',
+    address:      'Benjin Central Communist Park',
+    phone_number: '04 43 12 45 46',
+    category:     'chinese'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
